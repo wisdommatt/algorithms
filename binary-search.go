80397c4acef7bb86460c7bbd9e905919bae12a45
Wisdom Matthew <talk2wisdommatt@gmail.com>
2021-12-05 00:31:54 +0100

    feat: add binary search algorithm implementation

---
+++ binary-search.go
@@ -0,0 +1,24 @@
+package algorithms
+
+// BinarySearch performs a binary search on the array to
+// find the position of the given element.
+func BinarySearch(arr []int, val int) int {
+	arrLen := len(arr)
+	return binarySearchRecursive(arr, val, 0, arrLen)
+}
+
+// binarySearchRecursive is a helper function to perform a recursive
+// binary search on an array.
+func binarySearchRecursive(arr []int, val, left, right int) int {
+	mid := (left + right) / 2
+	if mid >= len(arr) || right < 0 {
+		return -1
+	}
+	if arr[mid] == val {
+		return mid
+	}
+	if val < arr[mid] {
+		return binarySearchRecursive(arr, val, left, mid-1)
+	}
+	return binarySearchRecursive(arr, val, mid+1, right)
+}
